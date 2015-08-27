aaca5441feec420de0f252e1266eefd32983fdbf
Natac13 <Natac13@user.noreply.github.com>
2015-08-27 17:26:58 -0400

    Not using a object anymore with a prototype since it was not working. Working with functions should be find in this situation.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
 
 /**
- * returns a list of all the values from the extra loads
- * @return {list}
+ * Finds all the extra-load classes from the DOM including newly created ones.
+ * @return {list} Extra load values.
  */
   function getExtraLoads() {
     var temArr = [];
     $('.extra-load').each(function () {
       temArr.push($(this).val());
     });
-    console.log(temArr);
     return temArr;
   }
 
+/**
+ * Will insert a new Extra Load input field to the bottom of the others.
+ */
   function addExtraLoad() {
     $('.extra-loads-wrapper').append('<div class="form-group"><label ' +
       'for="extra-load" class="col-sm-2">Extra Load W</label><div ' +
@@ -21,52 +23,58 @@ $(document).ready(function () {
   }
 
 /**
- * Object to hold the values. The prototype will have all the functions for
- * calculating the Unit demand
- * @type {Object}
+ * Given an area will calculate rule 8-202(1)(a)(i)(ii)(iii)
+ * @param  {number} area From the input field #area
+ * @return {number}      demand based off the area.
  */
-  var unitCalculator = {};
-  unitCalculator.prototype = {
-    calArea: function () {
-      if (this.area <= 45) {
-        return 3500;
-      }
-      if (this.area > 45 && this.area <= 90) {
-        return 5000;
-      }
-      var multiplier = Math.ceil((this.area - 90) / 90);
-      return multiplier > 1 ? 5000 + (1000 * multiplier) : 6000;
-    },
-    calRange: function () {
-      if (this.range <= 12000) {
-        return 6000;
-      }
-      return 6000 + ((this.range - 12000) * 0.4);
-    },
-    checkAnswer: function () {
-      this.area = $('#area').val();
-      this.range = $('#range').val();
-      this.heat = $('#heat').val();
-      this.extras = getExtraLoads();
-      this.ac = $('#a-c').val();
-      this.suite_voltage = $('#suite-voltage').val();
-      this.suite_phase = $('suite-phase').val();
-      // start getting calculations
-      this.demands.area = this.calArea();
-      this.demands.range = this.calRange();
+  function calArea(area) {
+    if (area <= 45) {
+      return 3500;
+    }
+    if (area > 45 && area <= 90) {
+      return 5000;
+    }
+    var multiplier = Math.ceil((area - 90) / 90);
+    return multiplier > 1 ? 5000 + (1000 * multiplier) : 6000;
+  }
 
+/**
+ * Given a range in watts will calculate rule 8-202(1)(v)
+ * @param  {number} range From the input field #range
+ * @return {number}       demand based off the range.
+ */
+  function calRange(range) {
+    if (range <= 12000) {
+      return 6000;
+    }
+    return 6000 + ((range - 12000) * 0.4);
+  }
 
-    },
-    giveAnswer: function () {
-      // display answer
-      if (this.area === undefined) {  // will be undefined if checkAnswer not run
-        alert("You need to try an answer first!");
-      }
-    },
-    calculateSubTotal: function () {
+  function checkAnswer() {
+    var area = $('#area').val(),
+      range = $('#range').val(),
+      heat = $('#heat').val(),
+      extras = getExtraLoads(),
+      ac = $('#ac').val(),
+      suite_voltage = $('#volts').val(),
+      suite_phase = $('#phase').val()
+      demands = [];
+    console.log(calArea(area));
+    // add each total to the demands array
+    demands.push(calArea(area))
 
+  }
+
+  function giveAnswer() {
+    // display answer
+    if (this.area === undefined) {  // will be undefined if checkAnswer not run
+      alert("You need to try an answer first!");
     }
   };
+
+  function calculateSubTotal() {
+
+  }
 
 
   // get buttons
@@ -74,8 +82,8 @@ $(document).ready(function () {
     $answer = $('#answer'),
     $add_load = $('#add-load');
   // on clicks
-  $check.click(unitCalculator.checkAnswer);
-  $answer.click(unitCalculator.giveAnswer);
+  $check.click(checkAnswer);
+  $answer.click(giveAnswer);
   $add_load.click(addExtraLoad);
 
 
