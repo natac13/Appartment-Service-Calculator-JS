eb91b2e4bda76ba606593b3e0ea395544c873005
Natac13 <Natac13@user.noreply.github.com>
2015-08-27 20:58:44 -0400

    Have working calculation for the units and being able to check with an user answer.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
+/**
+
+  TODO:
+  - Make button shake like electrical current
+  - disable check and answer buttons if o value put it the user-answer field
+  - do something better than an alert for right answer.
+  - maybe have to check 3 times before get answer. with addition of not being
+  able to input same answer... do this with logging each answer in an array, or
+  object might be better to use a for in loop.
+  - finish owner panel input and finial calculation
+
+ */
+
+
 $(document).ready(function () {
 
   var demands = {},
-    entries = {};
+    entries = {},
+    answer;
 /**
  * Finds all the extra-load classes from the DOM including newly created ones.
  * @return {list} Extra load values.
@@ -30,12 +45,9 @@ $(document).ready(function () {
  * @return {number}      demand based off the area.
  */
   function calArea(area) {
-    if (area <= 45) {
-      return 3500;
-    }
-    if (area > 45 && area <= 90) {
-      return 5000;
-    }
+    area = parseInt(area, 10);
+    if (area <= 45) { return 3500; }
+    if (area > 45 && area <= 90) { return 5000; }
     var multiplier = Math.ceil((area - 90) / 90);
     return multiplier > 1 ? 5000 + (1000 * multiplier) : 6000;
   }
@@ -46,10 +58,8 @@ $(document).ready(function () {
  * @return {number}       demand based off the range.
  */
   function calRange(range) {
-    if (range <= 12000) {
-      return 6000;
-    }
-    return 6000 + ((range - 12000) * 0.4);
+    range = parseInt(range, 10);
+    return range <= 12000 ? 6000 : 6000 + ((range - 12000) * 0.4);
   }
 
 /**
@@ -67,35 +77,66 @@ $(document).ready(function () {
     });
   }
 
+  function calHeatAC(heat, ac) {
+    heat = parseInt(heat, 10);
+    ac = parseInt(ac, 10);
+    if (ac > heat) { return ac; }
+    if (heat <= 10000) { return heat; }
+    return parseInt(((heat - 10000) * 0.75) + 10000, 10);
+  }
+
+  function calMinWireAmpacity(wattage, voltage, phase) {
+    wattage = parseInt(wattage, 10);
+    voltage = parseInt(voltage, 10);
+    phase = parseInt(phase, 10);
+    return phase === 1 ? (wattage / voltage).toFixed(2) : (wattage /
+        (Math.sqrt(3) * voltage)).toFixed(2)
+  }
+
   function checkAnswer() {
     entries = {
       area: $('#area').val(),
       range: $('#range').val(),
       heat: $('#heat').val(),
       extras: getExtraLoads(),
-      ac: $('#ac').val(),
-      suite_voltage: $('#volts').val(),
-      suite_phase: $('#phase').val(),
+      ac: $('#a-c').val(),
+      suite_voltage: $('#suite-voltage').val(),
+      suite_phase: $('#suite-phase').val(),
+      user_answer: $('#user-answer').val(),
     };
-    console.log(entries.area);
+
     // add each total to the demands array
     demands.area = calArea(entries.area);
+    console.log("area: " + demands.area);
     demands.range = calRange(entries.range);
-    //demands.heat = calHeat(entries.heat);
+    console.log("range " + demands.range);
     demands.extras = calExtras(entries.extras);
-    console.log(demands.extras);
+    console.log("extra " + demands.extras);
+    demands.subTotal = demands.area + demands.range + demands.extras;
+    console.log("sub " + demands.subTotal);
+    demands.heat_ac = calHeatAC(entries.heat, entries.ac);
+    console.log("heat_ac " + demands.heat_ac);
+    demands.suiteTotal = parseInt(demands.heat_ac, 10) +
+      parseInt(demands.subTotal, 10);
+      console.log("total wattage  " + demands.suiteTotal);
+    // find answer!!
+    answer = calMinWireAmpacity(demands.suiteTotal, entries.suite_voltage,
+      entries.suite_phase);
 
+    console.log(demands.suiteTotal);
+    console.log(answer);
+
+    // check for right answer then do css magic!!
+    if( parseInt(answer, 10) === parseInt(entries.user_answer, 10)) {
+      alert("YEah buddy!");
+    }
   }
 
   function giveAnswer() {
     // display answer
-    if (this.area === undefined) {  // will be undefined if checkAnswer not run
+    if (area === undefined) {  // will be undefined if checkAnswer not run
       alert("You need to try an answer first!");
     }
-  }
-
-  function calculateSubTotal() {
-
   }
 
 
