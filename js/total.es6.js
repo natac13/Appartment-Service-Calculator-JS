dc26e2cafa1b2dabd307ef04dd3083794b58be1d
Natac13 <Natac13@user.noreply.github.com>
2015-08-29 12:37:57 -0400

    Started the total form. Right now I have the function to grab all the unit info and make an array in descending order of all the units.
    The methods chain I came up with I think shows I am starting to
    understand how things in JavaScript work

---
+++ js/total.es6.js
@@ -0,0 +1,84 @@
+/**
+
+  TODO:
+  - Could look into lodash to flatten the array instead of this method.
+  - Got all the units now.
+  - add button to add another unit type.
+  - finsih the rest lol
+
+ */
+
+
+
+
+
+Array.prototype.concatAll = function () {
+    var results = [];
+    this.forEach((inner_array) => {
+        results.push.apply(results, inner_array);
+    });
+    return results;
+};
+
+$(document).ready(() => {
+
+  let TOTAL = (() => {
+    let entries = {},
+      demands = {},
+      answer;
+
+/**
+ * tmp is an array of array that looks like [[12000,3],[15000,2],[10000,1]]
+ * first value is the demand of the unit and the second is how many
+ * will sort base off the first value then map to produce an array of just
+ * demands in order for how many of each on. normal sort since map will build
+ * in the reverse direction....
+ * map over the array of arrays to make array of arrays this time each inner
+ * array has an element of the demand for how many unit there were(second value)
+ * afterward use concatAll() to flatten the array.
+ * highest demand first!!!
+ * @return {array} demands for all the units in order.
+ */
+    function getUnits() {
+      let tmp = [],
+        $x = $('#total-form');
+      $x.find('.unit-info').each(function() {
+        tmp.push([parseInt($(this).find("input[name='demand']").val(), 10),
+          parseInt($(this).find("input[name='count']").val(), 10)]);
+      });
+
+      // console.log(tmp.sort((a, b) => {
+      //         if (a[0] === b[0]) { return 0; }
+      //         return b[0] - a[0];
+      //       }).map((unitArray) => {
+      //   let ans = [];
+      //   for(let i = 0; i < unitArray[1]; i++) {
+      //     ans.push(unitArray[0]);
+      //   }
+      //   return ans;
+      // }).concatAll());
+      return tmp.sort((a, b) => {
+              if (a[0] === b[0]) { return 0; }
+              return b[0] - a[0];
+            }).map((unitArray) => {
+        let ans = [];
+        for(let i = 0; i < unitArray[1]; i++) {
+          ans.push(unitArray[0]);
+        }
+        return ans;
+      }).concatAll();
+
+    }
+
+
+    function calculator() {
+      entries.units = []  // built by function lol
+    }
+
+
+
+    $('#total-check').click(getUnits)
+
+
+  }());
+});
